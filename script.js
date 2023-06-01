931186dc0f1a6ec40e2c049dd1735b358ffdf503
Hariharan <110547087+hariharan-cs21@users.noreply.github.com>
2023-06-01 14:25:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,7 @@
-console.log("%cஇங்க என்ன பா வேல", "font-size: 50px")
-const passwordInput = document.querySelector(".password-box input"),
-  copyIcon = document.querySelector(".password-box .copy-icon"),
-  rangeInput = document.querySelector(".range-box input"),
-  sliderNumber = document.querySelector(".range-box .slider-number"),
-  generateButton = document.querySelector(".generate-button");
-
-var allCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789#@";
-const generatePassword = () => {
-  var newPassword = "";
-  for (var i = 0; i < rangeInput.value; i++) {
-    var randomNumbers = Math.floor(Math.random() * allCharacters.length);
-    newPassword += allCharacters[randomNumbers];
+var arr = [1, 2, 3, 4, 5, 6, 'a', 'b', 'c', 'd'];
+  var password = "";
+  for (var i = 0; i < 6; i++) {
+    var myIndex = Math.floor(Math.random() * arr.length);
+    password += arr[myIndex];
   }
-  passwordInput.value = newPassword;
-  copyIcon.classList.replace("uil-file-check-alt", "uil-copy");
-};
-rangeInput.addEventListener("input", () => {
-  sliderNumber.innerText = rangeInput.value;
-  generatePassword();
-});
-copyIcon.addEventListener("click", () => {
-  navigator.clipboard.writeText(passwordInput.value);
-  copyIcon.classList.replace("uil-copy", "uil-file-check-alt");
-  alert("Code copied")
-});
-generatePassword();
-generateButton.addEventListener("click", generatePassword);
+  document.getElementById("number").innerHTML =password
